f01ebdea6842c3ff1ca8d182f2c1ea1f22cd3bb9
mahalocode <you@example.com>
2016-09-10 20:03:33 +0000

    styling instructor page

--- db/migrate/20160910193128_alter_lessons_add_row_order.rb
+++ db/migrate/20160910193128_alter_lessons_add_row_order.rb
@@ -1,6 +1,5 @@
 class AlterLessonsAddRowOrder < ActiveRecord::Migration
   def change
   	add_column :lessons, :row_order, :integer
-  	add_index :lessons, :row_order
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910043312) do
+ActiveRecord::Schema.define(version: 20160910193128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160910043312) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
