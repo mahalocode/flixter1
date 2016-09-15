08875800a7c686c3a40f648f2809f5b8941cba64
mahalocode <you@example.com>
2016-09-15 01:33:32 +0000

    finalizing

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -34,6 +34,6 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def course_params
-    params.require(:course).permit(:title, :description, :cost, :image)
+    params.require(:course).permit(:title, :description, :cost, :image, :video)
   end
 end
